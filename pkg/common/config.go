1f7e6e052e0ef77ffc6b67c55724b1d0224ab141
Maya Barnea <mayab@il.ibm.com>
2025-07-28 12:17:11 +0300

    Add command line and yaml config file parameter 'enable-kvcache'
    Signed-off-by: Maya Barnea <mayab@il.ibm.com>

--- pkg/common/config.go
+++ pkg/common/config.go
@@ -168,6 +168,7 @@ func newConfig() *Configuration {
 		MinToolCallArrayParamLength:         1,
 		ToolCallNotRequiredParamProbability: 50,
 		ObjectToolCallNotRequiredParamProbability: 50,
+		EnableKVCache: false,
 	}
 }
 
