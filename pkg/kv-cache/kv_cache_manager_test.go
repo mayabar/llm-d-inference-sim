8329fbeaed0df984f126ed9dd1adca268e5605bc
Maya Barnea <mayab@il.ibm.com>
2025-07-30 12:23:36 +0300

    update kv cache test to be more modular
    Signed-off-by: Maya Barnea <mayab@il.ibm.com>

--- pkg/kv-cache/kv_cache_manager_test.go
+++ pkg/kv-cache/kv_cache_manager_test.go
@@ -27,114 +27,187 @@ const (
 	req3ID = "req3"
 )
 
+type ActionType int
+
+const (
+	actionStartRequest ActionType = iota
+	actionFinishRequest
+)
+
+type testRequest struct {
+	id     string
+	blocks []uint64
+}
+
+type expectedBlockInfo struct {
+	exists   bool
+	refCount int
+}
+
+type testAction struct {
+	action                 ActionType
+	request                testRequest
+	isError                bool
+	errMsg                 string
+	expectedActiveRequests int
+	expectedTotalBlocks    int
+	expectedUnusedBlocks   int
+	expectedBlocksInfo     map[uint64]expectedBlockInfo
+}
+
+func newTestAction(action ActionType, request testRequest) testAction {
+	return testAction{
+		action:                 action,
+		request:                request,
+		isError:                false,
+		expectedActiveRequests: -1,
+		expectedTotalBlocks:    -1,
+		expectedUnusedBlocks:   -1,
+	}
+}
+func newInvalidTestAction(action ActionType, request testRequest, errMsg string) testAction {
+	return testAction{
+		action:                 action,
+		request:                request,
+		isError:                true,
+		errMsg:                 errMsg,
+		expectedActiveRequests: -1,
+		expectedTotalBlocks:    -1,
+		expectedUnusedBlocks:   -1,
+	}
+}
+func newTestActionWithExpectedValues(action ActionType, request testRequest, expectedActiveRequests int,
+	expectedTotalBlocks int, expectedUnusedBlocks int, expectedBlocksInfo map[uint64]expectedBlockInfo) testAction {
+	return testAction{
+		action:                 action,
+		request:                request,
+		isError:                false,
+		expectedActiveRequests: expectedActiveRequests,
+		expectedTotalBlocks:    expectedTotalBlocks,
+		expectedUnusedBlocks:   expectedUnusedBlocks,
+		expectedBlocksInfo:     expectedBlocksInfo,
+	}
+}
+
+type testCase struct {
+	name      string
+	cacheSize int
+	actions   []testAction
+}
+
 var _ = Describe("KV cache", func() {
 	Context("blocks cache tests", func() {
-		It("single request", func() {
-			// check single request processing, ensure cache is valid after request processing started
-			// and after the processing was finished
-			blockCache := newBlockCache(3)
-			err := blockCache.startRequest(req1ID, []uint64{1, 2})
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks := blockCache.getStats()
-			Expect(activeRequests).To(Equal(1))
-			Expect(totalBlocks).To(Equal(2))
-			Expect(unusedBlocks).To(Equal(0))
+		// check single request processing, ensure cache is valid after request processing started
+		// and after the processing was finished
+		req1 := testRequest{req1ID, []uint64{1, 2}}
+		req2 := testRequest{req2ID, []uint64{3, 4}}
+		req2_1 := testRequest{req2ID, []uint64{1, 3}}
+		req3 := testRequest{req3ID, []uint64{5, 6}}
 
-			err = blockCache.finishRequest(req1ID)
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks = blockCache.getStats()
-			Expect(activeRequests).To(Equal(0))
-			Expect(totalBlocks).To(Equal(2))
-			Expect(unusedBlocks).To(Equal(2))
-		})
-		It("two requests", func() {
-			blockCache := newBlockCache(5)
+		testCases := []testCase{{
+			name:      "single request",
+			cacheSize: 3,
+			actions: []testAction{
+				newTestActionWithExpectedValues(actionStartRequest, req1, 1, 2, 0, nil),
+				newTestActionWithExpectedValues(actionFinishRequest, req1, 0, 2, 2, nil),
+			},
+		}, {
+			name:      "two requests",
+			cacheSize: 5,
+			actions: []testAction{
+				newTestAction(actionStartRequest, req1),
+				newTestActionWithExpectedValues(actionStartRequest, req2, 2, 4, 0, nil),
+				newTestActionWithExpectedValues(actionFinishRequest, req1, 1, 4, 2, nil),
+				newTestActionWithExpectedValues(actionFinishRequest, req2, 0, 4, 4, nil),
+			},
+		}, {
+			name:      "reusing blocks",
+			cacheSize: 5,
+			actions: []testAction{
+				newTestAction(actionStartRequest, req1),
+				// Check block '1' reference count (should be 2)
+				newTestActionWithExpectedValues(actionStartRequest, req2_1, 2, 3, 0, map[uint64]expectedBlockInfo{1: {true, 2}}),
+				// Check block '1' reference count (should be 1)
+				newTestActionWithExpectedValues(actionFinishRequest, req1, 1, 3, 1, map[uint64]expectedBlockInfo{1: {true, 1}}),
+			},
+		}, {
+			name:      "block eviction",
+			cacheSize: 4,
+			actions: []testAction{
+				newTestAction(actionStartRequest, req1),
+				newTestAction(actionStartRequest, req2),
+				newTestActionWithExpectedValues(actionFinishRequest, req2, -1, -1, -1, map[uint64]expectedBlockInfo{3: {true, 0}}),
+				newTestActionWithExpectedValues(actionStartRequest, req3, -1, -1, -1, map[uint64]expectedBlockInfo{
+					3: {false, -1},
+					5: {true, 1},
+				}),
+			},
+		}, {
+			name:      "cache full, no eviction",
+			cacheSize: 4,
+			actions: []testAction{
+				newTestAction(actionStartRequest, req1),
+				newTestAction(actionStartRequest, req2),
+				newInvalidTestAction(actionStartRequest, req3, "cache is full and no blocks available for eviction"),
+			},
+		}}
 
-			err := blockCache.startRequest(req1ID, []uint64{1, 2})
-			Expect(err).NotTo(HaveOccurred())
-			err = blockCache.startRequest(req2ID, []uint64{3, 4})
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks := blockCache.getStats()
-			Expect(activeRequests).To(Equal(2))
-			Expect(totalBlocks).To(Equal(4))
-			Expect(unusedBlocks).To(Equal(0))
+		for _, test := range testCases {
+			It(test.name, func() {
+				blockCache := newBlockCache(test.cacheSize)
 
-			err = blockCache.finishRequest(req1ID)
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks = blockCache.getStats()
-			Expect(activeRequests).To(Equal(1))
-			Expect(totalBlocks).To(Equal(4))
-			Expect(unusedBlocks).To(Equal(2))
+				for _, action := range test.actions {
+					var err error
 
-			err = blockCache.finishRequest(req2ID)
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks = blockCache.getStats()
-			Expect(activeRequests).To(Equal(0))
-			Expect(totalBlocks).To(Equal(4))
-			Expect(unusedBlocks).To(Equal(4))
-		})
-		It("reusing blocks", func() {
-			blockCache := newBlockCache(5)
+					switch action.action {
+					case actionStartRequest:
+						err = blockCache.startRequest(action.request.id, action.request.blocks)
+					case actionFinishRequest:
+						err = blockCache.finishRequest(action.request.id)
+					}
 
-			err := blockCache.startRequest(req1ID, []uint64{1, 2})
-			Expect(err).NotTo(HaveOccurred())
+					if action.isError {
+						Expect(err).To(HaveOccurred())
+						if len(action.errMsg) > 0 {
+							Expect(err.Error()).To(Equal(action.errMsg))
+						}
+						continue
+					}
 
-			err = blockCache.startRequest(req2ID, []uint64{1, 3})
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks := blockCache.getStats()
-			Expect(activeRequests).To(Equal(2))
-			Expect(totalBlocks).To(Equal(3))
-			Expect(unusedBlocks).To(Equal(0))
-			// Check block '1' reference count (should be 2)
-			info, exists := blockCache.getBlockInfo(1)
-			Expect(exists).To(BeTrue())
-			Expect(info.refCount).To(Equal(2))
+					// ensure that error does not accured
+					Expect(err).NotTo(HaveOccurred())
 
-			err = blockCache.finishRequest(req1ID)
-			Expect(err).NotTo(HaveOccurred())
-			activeRequests, totalBlocks, unusedBlocks = blockCache.getStats()
-			Expect(activeRequests).To(Equal(1))
-			Expect(totalBlocks).To(Equal(3))
-			Expect(unusedBlocks).To(Equal(1))
-			// Check block '1' reference count (should be 1)
-			info, exists = blockCache.getBlockInfo(1)
-			Expect(exists).To(BeTrue())
-			Expect(info.refCount).To(Equal(1))
+					// check cache info if required
+					if action.expectedActiveRequests >= 0 || action.expectedTotalBlocks >= 0 || action.expectedUnusedBlocks >= 0 {
+						activeRequests, totalBlocks, unusedBlocks := blockCache.getStats()
+						if action.expectedActiveRequests >= 0 {
+							Expect(activeRequests).To(Equal(action.expectedActiveRequests))
+						}
+						if action.expectedTotalBlocks >= 0 {
+							Expect(totalBlocks).To(Equal(action.expectedTotalBlocks))
+						}
+						if action.expectedUnusedBlocks >= 0 {
+							Expect(unusedBlocks).To(Equal(action.expectedUnusedBlocks))
+						}
+					}
 
-		})
-		It("block eviction", func() {
-			// text eviction
-			blockCache := newBlockCache(4)
-
-			err := blockCache.startRequest(req1ID, []uint64{1, 2})
-			Expect(err).NotTo(HaveOccurred())
-			err = blockCache.startRequest(req2ID, []uint64{3, 4})
-			Expect(err).NotTo(HaveOccurred())
-			err = blockCache.finishRequest(req2ID)
-			Expect(err).NotTo(HaveOccurred())
-			info, exists := blockCache.getBlockInfo(3)
-			Expect(exists).To(BeTrue())
-			Expect(info.refCount).To(Equal(0))
-			err = blockCache.startRequest(req3ID, []uint64{5, 6})
-			Expect(err).NotTo(HaveOccurred())
-			_, exists = blockCache.getBlockInfo(3)
-			Expect(exists).To(BeFalse())
-			info, exists = blockCache.getBlockInfo(5)
-			Expect(exists).To(BeTrue())
-			Expect(info.refCount).To(Equal(1))
-		})
-		It("cache full, no eviction", func() {
-			// text eviction
-			blockCache := newBlockCache(4)
-
-			err := blockCache.startRequest(req1ID, []uint64{1, 2})
-			Expect(err).NotTo(HaveOccurred())
-			err = blockCache.startRequest(req2ID, []uint64{3, 4})
-			Expect(err).NotTo(HaveOccurred())
-			err = blockCache.startRequest(req3ID, []uint64{5, 6})
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("cache is full and no blocks available for eviction"))
-		})
+					// check specific blocks info if required
+					if len(action.expectedBlocksInfo) > 0 {
+						for block, expectedInfo := range action.expectedBlocksInfo {
+							info, exists := blockCache.getBlockInfo(block)
+							if expectedInfo.exists {
+								Expect(exists).To(BeTrue())
+							} else {
+								Expect(exists).To(BeFalse())
+							}
+							if expectedInfo.refCount >= 0 {
+								Expect(info.refCount).To(Equal(expectedInfo.refCount))
+							}
+						}
+					}
+				}
+			})
+		}
 	})
 })
 
